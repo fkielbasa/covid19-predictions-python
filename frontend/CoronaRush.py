9116e08f320f80a55e4df1be340383d1d43346c0
DwiN3 <dwinicht333@gmail.com>
2024-05-07 21:05:51 +0200

    Dodanie kolorków

--- frontend/CoronaRush.py
+++ frontend/CoronaRush.py
@@ -23,41 +23,45 @@ root.title("Corona Rush")
 window_width, window_height = calculate_window_size(root.winfo_screenwidth(), root.winfo_screenheight())
 center_window(root, window_width, window_height)
 
+# Kolory
+background_color = "#344955"
+chart_color = "#4a6572"
+orange_color = "#f9aa33"
+
 # Lewy panel
-left_frame = tk.Frame(root, bg="white", width=300, height=window_height)
+left_frame = tk.Frame(root, bg=background_color, width=350, height=window_height)
 left_frame.pack(side=tk.LEFT, fill=tk.Y)
 
 # Tytuł aplikacji
-label = tk.Label(left_frame, text="Corona Rush", font=("Arial", 18, "bold"), bg="white")
-label.place(relx=0.5, rely=0.03, anchor=tk.CENTER)
+label = tk.Label(left_frame, text="Corona Rush", font=("Arial", 25, "bold"), fg="white", bg=background_color)
+label.place(relx=0.5, rely=0.06, anchor=tk.CENTER)
 
 # Opis selectora
-label = tk.Label(left_frame, text="Wybierz państwo", font=("Arial", 13), bg="white")
-label.place(relx=0.11, rely=0.08)
+label = tk.Label(left_frame, text="Wybierz państwo", font=("Arial", 13), fg="white", bg=background_color)
+label.place(relx=0.14, rely=0.15)
 
 # Selektor
 countries = ["Polska", "Anglia", "Niemcy", "Rosja"]
 selected_country = tk.StringVar(left_frame)
 selected_country.set(countries[0])  # Ustawienie domyślnego wyboru
 country_menu = tk.OptionMenu(left_frame, selected_country, *countries, command=country_selected)
-country_menu.config(width=30)
-country_menu.place(relx=0.5, rely=0.13, anchor=tk.CENTER)
+country_menu.config(width=35)
+country_menu.place(relx=0.5, rely=0.20, anchor=tk.CENTER)
 
 # Separator
-canvas = tk.Canvas(root, width=1, height=window_height, bg="black", highlightthickness=0)
-canvas.create_line(1, 0, 1, window_height, fill="black")
+canvas = tk.Canvas(root, width=8, height=window_height, bg=orange_color, highlightthickness=0)
 canvas.pack(side=tk.LEFT, fill=tk.Y)
 
 # Prawy panel
-right_frame = tk.Frame(root, bg="white", width=window_width-301, height=window_height)
+right_frame = tk.Frame(root, bg=background_color, width=window_width-301, height=window_height)
 right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
 # Opis wykresu
-label = tk.Label(right_frame, text="Wykres", font=("Arial", 14), bg="white")
-label.place(relx=0.5, rely=0.02, anchor=tk.CENTER)
+label = tk.Label(right_frame, text="Wykres", font=("Arial", 14), fg="white", bg=background_color)
+label.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
 
 # Ramka dla wykresu
-chart_frame = tk.Frame(right_frame, bg="grey", width=window_width-301-200, height=window_height-150, highlightthickness=2, highlightbackground="black")
+chart_frame = tk.Frame(right_frame, bg=chart_color, width=window_width-501, height=window_height-150, highlightthickness=5, highlightbackground=orange_color)
 chart_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 root.mainloop()

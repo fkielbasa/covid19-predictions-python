537f98209b1e7bf49bb5e7d8c484df081dca699f
Narothe <kuba.dojka@gmail.com>
2024-05-13 23:15:04 +0200

    Update CoronaRush.py

--- frontend/CoronaRush.py
+++ frontend/CoronaRush.py
@@ -49,24 +49,41 @@ left_frame.pack(side=tk.LEFT, fill=tk.Y)
 countries = ["Polska", "Anglia", "Niemcy", "Rosja"]
 selected_country = tk.StringVar(left_frame)
 selected_country.set(countries[0])  # Ustawienie domyślnego wyboru
-country_menu = tk.OptionMenu(left_frame, selected_country, *countries, command=country_selected)
-country_menu.config(width=40)
-country_menu.place(relx=0.5, rely=0.07, anchor=tk.CENTER)
+# country_menu = tk.OptionMenu(left_frame, selected_country, *countries, command=country_selected)
+# country_menu.config(width=40)
+# country_menu.place(relx=0.5, rely=0.07, anchor=tk.CENTER)
+menu_width = 40
+menu = tk.OptionMenu(left_frame, selected_country, *countries, command=country_selected)
+menu.config(width=menu_width)
+menu.place(relx=0.5, rely=0.07, anchor=tk.CENTER)
+# menu.pack(pady=10)
 
 # Separator
-canvas = tk.Canvas(root, width=8, height=window_height, bg=orange_color, highlightthickness=0)
-canvas.pack(side=tk.LEFT, fill=tk.Y)
+# canvas = tk.Canvas(root, width=8, height=window_height, bg=orange_color, highlightthickness=0)
+# canvas.pack(side=tk.LEFT, fill=tk.Y)
 
 # Prawy panel
-right_frame = tk.Frame(root, bg=background_color, width=window_width-301, height=window_height)
+right_frame = tk.Frame(root, bg=background_color, width=window_width-300, height=window_height)
 right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
 # Opis wykresu
-label = tk.Label(right_frame, text="Wykres", font=("Arial", 14), fg="white", bg=background_color)
-label.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
+# label = tk.Label(right_frame, text="Wykres", font=("Arial", 14), fg="white", bg=background_color)
+# label.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
 
 # Ramka dla wykresu
-chart_frame = tk.Frame(right_frame, bg=chart_color, width=window_width-501, height=window_height-150, highlightthickness=5, highlightbackground=orange_color)
+# chart_frame = tk.Frame(right_frame, bg=chart_color, width=window_width-500, height=window_height-150, highlightthickness=5, highlightbackground=orange_color)
+# chart_frame.place(relx=0.5, rely=0.41, anchor=tk.CENTER)
+
+# Wymiary wykresu
+chart_width = 600
+chart_height = 500
+
+# Współrzędne wykresu
+chart_x = (window_width - chart_width) / 2
+chart_y = (window_height - chart_height) / 2
+
+# Ramka dla wykresu
+chart_frame = tk.Frame(right_frame, bg=chart_color, width=chart_width, height=chart_height, highlightthickness=5, highlightbackground=orange_color)
 chart_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 root.mainloop()

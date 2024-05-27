c480032d88100477439db140465e8b51a3329a81
Narothe <kuba.dojka@gmail.com>
2024-05-27 21:59:11 +0200

    feat: show alerts, know how to separate files

--- frontend/CoronaRush.py
+++ frontend/CoronaRush.py
@@ -7,7 +7,11 @@ import numpy as np
 from scipy.interpolate import interp1d
 import datetime
 
-customtkinter.set_appearance_mode("System")
+from WindowSettings import calculate_window_size
+from WindowSettings import center_window
+
+
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
 root = customtkinter.CTk()
@@ -17,18 +21,6 @@ root.title("Corona Rush")
 root.iconbitmap("icon.ico")
 
 
-def calculate_window_size(screen_width, screen_height):
-    window_width = int(screen_width * 0.8)
-    window_height = int(screen_height * 0.8)
-    return window_width, window_height
-
-def center_window(window, width, height):
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-    x_coordinate = (screen_width - width) // 2
-    y_coordinate = (screen_height - height) // 2
-    window.geometry(f"{width}x{height}+{x_coordinate}+{y_coordinate}")
-
 def load_data(country):
     filename = f'data/CovidData{country}.json'
     try:
@@ -36,7 +28,7 @@ def load_data(country):
             data = json.load(file)
         return data['data']
     except FileNotFoundError:
-        print(f"File {filename} not found.")
+        log_message(f"File {filename} not found.")
         return []
 
 def plot_data(data, frame, country):
@@ -88,6 +80,39 @@ def show_waiting_message():
     waiting_label = customtkinter.CTkLabel(right_frame, text="Waiting for data...", font=("Arial", 20))
     waiting_label.place(relx=0.5, rely=0.5, anchor="center")
 
+def log_message(message):
+    alert_label.configure(text=message)
+    print(message)
+
+def on_submit_dates(dates):
+    try:
+        # Konwersja dat na obiekt datetime
+        date1 = datetime.datetime.strptime(dates[0], "%d-%m-%Y")
+        date2 = datetime.datetime.strptime(dates[1], "%d-%m-%Y")
+        date3 = datetime.datetime.strptime(dates[2], "%d-%m-%Y")
+
+        # Sprawdzenie warunków
+        if date2 <= date1 or (date2 - date1).days < 7:
+            log_message("Druga data musi być co najmniej 7 dni późniejsza od pierwszej.")
+            return
+        if date3 <= date1 or date3 <= date2:
+            log_message("Trzecia data musi być późniejsza niż pierwsza i druga.")
+            return
+
+        formatted_dates = [date1.strftime("%Y-%m-%d"), date2.strftime("%Y-%m-%d"), date3.strftime("%Y-%m-%d")]
+        for date in formatted_dates:
+            log_message(date)
+
+        selected_country = listbox.get(listbox.curselection())
+        data = load_data(selected_country)
+        filtered_data = [entry for entry in data if entry['day'] in formatted_dates]
+        if filtered_data:
+            plot_data(filtered_data, right_frame, selected_country)
+        else:
+            show_waiting_message()
+
+    except ValueError as e:
+        log_message(f"Error parsing dates: {e}")
 
 # Obliczenie i ustawienie rozmiaru okna na podstawie rozmiaru ekranu
 window_width, window_height = calculate_window_size(root.winfo_screenwidth(), root.winfo_screenheight())
@@ -95,15 +120,15 @@ root.geometry(f"{window_width}x{window_height}")
 center_window(root, window_width, window_height)
 
 # Prawy panel
-right_frame = customtkinter.CTkFrame(root, width=800, height=window_height-100, border_width=5, border_color="#F9AA33")
-right_frame.place(relx=0.62, rely=0.47, anchor="center")
+right_frame = customtkinter.CTkFrame(root, width=800, height=window_height-200, border_width=5, border_color="#F9AA33")
+right_frame.place(relx=0.62, rely=0.45, anchor="center")
 
 # Początkowa wiadomość "Waiting for data..."
 show_waiting_message()
 
 # Lewy panel
-left_frame = customtkinter.CTkFrame(root, width=300, height=window_height)
-left_frame.place(relx=0.145, rely=0.33, anchor="center")
+left_frame = customtkinter.CTkFrame(root, width=300, height=window_height-100)
+left_frame.place(relx=0.145, rely=0.3, anchor="center")
 
 # Lista rozwijalna w ramce
 listbox = CTkListbox(left_frame, command=on_country_selected, width=250, height=150)
@@ -144,34 +169,12 @@ date_entry3.grid(row=2, column=1, padx=5)
 submit_button = customtkinter.CTkButton(left_frame, text="Submit Dates", command=lambda: on_submit_dates([date_entry1.get(), date_entry2.get(), date_entry3.get()]))
 submit_button.pack(pady=10)
 
-def on_submit_dates(dates):
-    try:
-        # Konwersja dat na obiekt datetime
-        date1 = datetime.datetime.strptime(dates[0], "%d-%m-%Y")
-        date2 = datetime.datetime.strptime(dates[1], "%d-%m-%Y")
-        date3 = datetime.datetime.strptime(dates[2], "%d-%m-%Y")
+# Ramka dla alertów
+alert_frame = customtkinter.CTkFrame(root, height=50, fg_color="#242424")
+alert_frame.pack(fill='x', side='bottom')
 
-        # Sprawdzenie warunków
-        if date2 <= date1 or (date2 - date1).days < 7:
-            print("Druga data musi być co najmniej 7 dni późniejsza od pierwszej.")
-            return
-        if date3 <= date1 or date3 <= date2:
-            print("Trzecia data musi być późniejsza niż pierwsza i druga.")
-            return
-
-        formatted_dates = [date1.strftime("%Y-%m-%d"), date2.strftime("%Y-%m-%d"), date3.strftime("%Y-%m-%d")]
-        for date in formatted_dates:
-            print(date)
-
-        selected_country = listbox.get(listbox.curselection())
-        data = load_data(selected_country)
-        filtered_data = [entry for entry in data if entry['day'] in formatted_dates]
-        if filtered_data:
-            plot_data(filtered_data, right_frame, selected_country)
-        else:
-            show_waiting_message()
-
-    except ValueError as e:
-        print(f"Error parsing dates: {e}")
+# Etykieta dla wyświetlania alertów
+alert_label = customtkinter.CTkLabel(alert_frame, text="", font=("Arial", 14))
+alert_label.pack(pady=5)
 
 root.mainloop()

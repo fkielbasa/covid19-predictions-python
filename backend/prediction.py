4e7645de80f990c78d51ca1c84e0426229e7859c
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-06-24 22:39:39 +0200

    feat: predictions plots

--- backend/prediction.py
+++ backend/prediction.py
@@ -4,7 +4,7 @@ import numpy as np
 from backend.predictive import *
 
 
-def nowy3(data, future_date):
+def make_predicition(data):
     """Funkcja przeprowadzająca eksperyment predykcji."""
     warnings.filterwarnings("ignore")
 
@@ -31,17 +31,11 @@ def nowy3(data, future_date):
     xlabel = "data"
     title = "covidowe przypadki"
 
-    visualize_plot_scatter1 = visualize_plot_scatter(
-        x_plot=None, y_plot=None, x_scatter=df["date"], y_scatter=df.loc[:, y_feature],
-        title=title, xlabel=xlabel, ylabel=ylabel, plot_color="orange", scatter_color="green"
-    )
 
     df_train = df[pd.to_datetime(df.date) < pd.to_datetime("2022-01-01")]
-
     df_test = df[pd.to_datetime(df.date) >= pd.to_datetime("2022-01-01")]
 
     visualize3 = visualize(df=df_train, x="date", y=y_feature, title=title + "\n{Zbiór UCZĄCY}", regression=False)
-
     visualize4 = visualize(df=df_train, x="date", y=y_feature,
                            title=title + " & " + "Regresja" + "\n{Zbiór UCZĄCY}",
                            regression=True)
@@ -58,6 +52,7 @@ def nowy3(data, future_date):
         x_plot=x_dates, y_plot=y, x_scatter=df_train["date"], y_scatter=y_train,
         title=chart_title, xlabel=xlabel, ylabel=ylabel, plot_color="orange", scatter_color="green"
     )
+
     x_test = np.array(df_test[x_feature]).reshape((-1, 1))
     y_test = np.array(df_test[y_feature])
     df_prediction, _ = prediction(model, x_test, y_test)
@@ -70,21 +65,4 @@ def nowy3(data, future_date):
         title=chart_title, xlabel=xlabel, ylabel=ylabel, plot_color="orange", scatter_color="green"
     )
 
-    last_date = df['date'].max()
-    future_timestamps = pd.date_range(start=last_date, end=future_date).map(datetime.datetime.timestamp)
-
-    x_future = np.array(future_timestamps).reshape((-1, 1))
-    y_future_pred = model.predict(x_future)
-    x_future_dates = [datetime.datetime.fromtimestamp(ts) for ts in future_timestamps]
-
-    chart_title = "PROGNOZA NA PRZYSZŁOŚĆ\n" + title + " & " + "Regresja"
-    visualize_plot_scatter4 = visualize_plot_scatter(
-        x_plot=x_future_dates, y_plot=y_future_pred, x_scatter=None, y_scatter=None,
-        title=chart_title, xlabel=xlabel, ylabel=ylabel, plot_color="orange", scatter_color="blue"
-    )
-
-    return [visualize_plot_scatter4, visualize_plot_scatter2, visualize_plot_scatter3, visualize_plot_scatter1]
-
-
-if __name__ == "__main__":
-    pass
+    return [visualize_plot_scatter2, visualize_plot_scatter3, visualize3, visualize4]
